e48ca598c3e2fa3717f11efda93adfab9b1d7008
Aung Kyaw Htwe <dev.aungkyawhtwe@gmail.com>
2022-09-29 21:07:30 +0630

    initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$title="Test 1 Title";
+?>
+<style>
+    .text-center{
+        text-align:center;
+    }
+    .py{
+        padding:30px;
+    }
+</style>
+<h2 class="py text-center"><?= $title; ?></h2>
