9ac12c83eae389ebb8b859481f1d60f97372a29b
Aung Kyaw Htwe <dev.aungkyawhtwe@gmail.com>
2022-09-29 21:42:27 +0630

    initial commit project

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '113fc152cb7f09d9f10aa29f03412081b2327645',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '113fc152cb7f09d9f10aa29f03412081b2327645',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
