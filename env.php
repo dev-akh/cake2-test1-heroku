dd1b365a8a76af472a728bc23251dd1229ffc23f
Aung Kyaw Htwe <dev.aungkyawhtwe@gmail.com>
2022-09-29 21:20:53 +0630

    initial commit project

---
+++ env.php
@@ -0,0 +1,11 @@
+<?php 
+
+define('ENV_APP_NAME','DewTouch');
+define('ENV_SITETITLE','DewTouch');
+
+define('ENV_DEBUG_LEVEL',2);
+
+define('ENV_DB_HOST','sql6.freemysqlhosting.net');
+define('ENV_DB_USERNAME','sql6522819');
+define('ENV_DB_PASSWORD','rirh1qAaMs');
+define('ENV_DB_NAME','sql6522819');
